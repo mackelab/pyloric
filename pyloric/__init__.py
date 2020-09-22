b196cdd572dc57843cfbfde6af679193b1c13bb7
michael <michael.deistler@t-online.de>
2020-09-22 17:51:40 +0200

    added energyscape to init

--- pyloric/__init__.py
+++ pyloric/__init__.py
@@ -1,4 +1,11 @@
-from pyloric.sbi_wrapper import get_time, simulate, stats, wrapper
+from pyloric.sbi_wrapper import (
+    get_time,
+    simulate,
+    stats,
+    wrapper,
+    simulate_energyscape,
+    simulate_general,
+)
 from pyloric.sbi_prior import create_prior, create_prior_general
 from pyloric.sbi_simulator import sim_time
 from pyloric.sbi_simulator_energyScape import sim_time_energyscape
